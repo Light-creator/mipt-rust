76cfd40f015a8813f7789e179f17afc4199a8a42
Light-creator <ibadulasha@ya.ru>
2024-10-01 08:45:31 +0300

    comm problem

--- std-collections/comm/src/main.rs
+++ std-collections/comm/src/main.rs
@@ -1,8 +1,32 @@
 #![forbid(unsafe_code)]
 
-// TODO: your code goes here.
+use std::{fs::File, io::BufRead, io::BufReader};
+use std::collections::HashSet;
+use std::io;
+use std::path::Path;
+
 
 fn main() {
-    // TODO: your code goes here.
-    unimplemented!()
+    let args = std::env::args().collect::<Vec<String>>();
+    let mut hash = HashSet::<String>::new();
+    
+    if let Ok(lines) = read_lines(args[1].clone()) {
+        for line in lines.flatten() {
+            hash.insert(line);
+        }
+    }
+
+    if let Ok(lines) = read_lines(args[2].clone()) {
+        for line in lines.flatten() {
+            if let Some(success) = hash.take(&line) {
+                println!("{success}");
+            }
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
 }
